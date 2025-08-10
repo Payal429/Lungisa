17eb70982e2ae08c2a96df049b5464d792db31b6
Payal429 <payal.sunil429@gmail.com>
2025-08-10 12:05:21 +0200

    Initial commit - setup Lungisa ASP.NET MVC with Firebase

---
+++ Lungisa/Firebase/FirebaseConfig.cs
@@ -0,0 +1,23 @@
+﻿using FirebaseAdmin;
+using Google.Apis.Auth.OAuth2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Lungisa.Firebase
+{
+    public class FirebaseConfig
+    {
+        public static void InitializeFirebase()
+        {
+            if (FirebaseApp.DefaultInstance == null)
+            {
+                FirebaseApp.Create(new AppOptions()
+                {
+                    Credential = GoogleCredential.FromFile(AppDomain.CurrentDomain.BaseDirectory + "App_Data/lungisa-firebase-key.json")
+                });
+            }
+        }
+    }
+}
---
+++ Lungisa/Services/FirebaseService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Firebase.Database;
+
+
+namespace Lungisa.Services
+{
+    public class FirebaseService
+    {
+        private FirebaseClient client;
+
+        public FirebaseService()
+        {
+            client = new FirebaseClient(
+                "https://lungisa-e03bd-default-rtdb.firebaseio.com/" // Replace with your actual DB URL
+            );
+        }
+    }
+}
